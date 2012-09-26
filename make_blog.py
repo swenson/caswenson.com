ca948729b138fd24dffd17a8d03974498b66e359
Christopher Swenson <chris@caswenson.com>
2012-09-26 00:11:01 -0700

    Add links to index post text

--- make_blog.py
+++ make_blog.py
@@ -5,6 +5,7 @@ import glob
 import os
 import os.path
 import shutil
+import string
 import subprocess as sp
 
 DISQUS_SHORTNAME = "swenson"
@@ -31,11 +32,20 @@ index_template = open("templates/index.html.template").read()
 
 posts_text = ""
 for mtime, post in posts:
-	posts_text += """<div id="post">%s</div>""" % post
+	lines = post.split("\n")
+	post_title = lines[0][4:-5]
+	link_title = ''.join([x for x in post_title.lower() if x in (string.ascii_lowercase + " ")]).strip().replace(" ", "_")
+	while "__" in link_title:
+		link_title = link_title.replace("__", "_")
+	link = "%s/%s" % (str(date.fromtimestamp(mtime)).replace("-", "/"), link_title)
+	post_h1 = "<h1><a href=\"%s\">%s</a></h1>" % (link, post_title)
+
+	posts_text += """<div id="post">%s%s</div>""" % (post_h1, "\n".join(lines[1:]))
 disqus_text = disqus_template % DISQUS_SHORTNAME
 
 static_files = [os.path.join(x[0], y) for x in os.walk("static") for y in x[2]]
 
+# Copy static files.
 if os.path.exists("public"):
 	shutil.rmtree("public")
 
@@ -43,5 +53,8 @@ for fname in static_files:
 	run_cmd("mkdir -p " + os.path.join("public", os.path.dirname(fname[len("static/"):])))
 	shutil.copy(fname, os.path.join("public", fname[len("static/"):]))
 
+# Create index.
 with open("public/index.html", "w") as index:
 	index.write(index_template.format(posts=posts_text, disqus=disqus_text))
+
+# Create posts.
