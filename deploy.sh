50ffc22e75d55a7b9a5248f96f247ae14771c7bf
Christopher Swenson <chris@caswenson.com>
2016-05-07 22:56:18 -0700

    Fix HTML types in S3

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-aws s3 sync public/ s3://caswenson.com/ --region us-west-2 --acl public-read
+aws s3 sync public/ s3://caswenson.com/ --exclude '20*' --exclude about --exclude archive --exclude contact --exclude feed --exclude mc --exclude rss --exclude speaking --region us-west-2 --acl public-read --content-disposition inline
+aws s3 sync public s3://caswenson.com/ --exclude '*' --include '20*' --include about --include archive --include contact --include feed --include mc --include rss --include speaking --region us-west-2 --acl public-read --content-type text/html
