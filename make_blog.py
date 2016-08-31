76d7fe4c2aeb066deb40950a985aacc737b14d50
Christopher Swenson <chris@caswenson.com>
2016-08-31 16:02:26 -0700

    Move public folder to docs

--- make_blog.py
+++ make_blog.py
@@ -52,13 +52,13 @@ static_files = [os.path.join(x[0], y) for x in os.walk("static") for y in x[2]]
 
 print "Cleaning"
 # Copy static files.
-if os.path.exists("public"):
-  shutil.rmtree("public")
+if os.path.exists("docs"):
+  shutil.rmtree("docs")
 
 print "Static files"
 for fname in static_files:
-  run_cmd("mkdir -p " + os.path.join("public", os.path.dirname(fname[len("static/"):])))
-  shutil.copy(fname, os.path.join("public", fname[len("static/"):]))
+  run_cmd("mkdir -p " + os.path.join("docs", os.path.dirname(fname[len("static/"):])))
+  shutil.copy(fname, os.path.join("docs", fname[len("static/"):]))
 
 months = 'xxx jan feb mar apr may jun jul aug sep oct nov dec'.split()
 
@@ -123,8 +123,8 @@ for mtime, post in posts:
   processed += 1
 
   archive_text += """<li><a href="%s">%s</a></li>""" % (link, post_title)
-  run_cmd("mkdir -p " + os.path.join("public", os.path.dirname(link)))
-  with open(os.path.join("public", link), "w") as post_out:
+  run_cmd("mkdir -p " + os.path.join("docs", os.path.dirname(link)))
+  with open(os.path.join("docs", link), "w") as post_out:
     url = "http://www.caswenson.com/" + link
     #disqus = """<div id="disqus_thread"></div><script type="text/javascript" src="http://disqus.com/forums/%s/embed.js"></script><noscript><a href="http://%s.disqus.com/?url=%s">View the discussion thread.</a></noscript><a href="http://disqus.com" class="dsq-brlink">blog comments powered by <span class="logo-disqus">Disqus</span></a>"""
     #disqus = disqus % (DISQUS_SHORTNAME, DISQUS_SHORTNAME, url)
@@ -136,24 +136,24 @@ archive_text += "</ul>"
 print "Index"
 
 # Create index.
-with open("public/index.html", "w") as index:
+with open("docs/index.html", "w") as index:
   index.write(index_template.format(title="Main", posts=posts_text, disqus=disqus_text))
 
 print "Archive"
 # Create archive at /archive
-with open("public/archive", "w") as archive:
+with open("docs/archive", "w") as archive:
   archive.write(index_template.format(title="Archive", posts=archive_text, disqus=""))
 
 print "Atom"
 # Create Atom
-with open("public/feed", "w") as rss:
+with open("docs/feed", "w") as rss:
   mtime = posts[0][0]
   modified_time = time.strftime("%Y-%m-%dT%H:%M:%S-00:00", time.gmtime(mtime))
   rss.write(rss_template % (modified_time, atom_text))
 
 print "RSS 2.0"
 # Create RSS
-with open("public/rss", "w") as rss:
+with open("docs/rss", "w") as rss:
   mtime = posts[0][0]
   modified_time = time.strftime("%a, %d %b %Y %H:%M:%S %z", time.gmtime(mtime))
   rss.write(rss_template % (modified_time, rss_text))
@@ -162,8 +162,5 @@ print "Pages"
 # Create Pages
 for page in glob.glob("pages/*"):
   name = os.path.basename(page)
-  with open("public/" + name, "w") as page_out:
+  with open("docs/" + name, "w") as page_out:
     page_out.write(index_template.format(title=name.replace("_", " ").capitalize(), posts=open(page).read(), disqus=""))
-
-
-
